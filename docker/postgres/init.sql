453a13f7f744cd9e6e3f79227d77408cb431d362
Mwapsam <mwapsam@gmail.com>
2025-07-21 16:36:07 +0200

    Update

---
+++ docker/postgres/init.sql
@@ -0,0 +1,5 @@
+CREATE USER digital_cards_user WITH PASSWORD 'Suguadsg@#yufgASDFTYEAFUYTDFFUYDHDSJH78327843Y273GQWghusdvd#@ugb';
+
+CREATE DATABASE digital_cards_db OWNER digital_cards_user;
+
+ALTER DATABASE digital_cards_db CONNECTION LIMIT -1;
