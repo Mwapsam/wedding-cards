2d756093ff340935827a71c16031d83e6fa53ca7
Mwapsam <mwapsam@gmail.com>
2025-07-22 16:29:14 +0200

    Update

--- wedding_res/settings.py
+++ wedding_res/settings.py
@@ -176,6 +176,9 @@ if DEBUG is False:
         "compressor.finders.CompressorFinder",
     ]
 
+    DEFAULT_FILE_STORAGE = "wedding_res.storage_backends.mediaRootS3Boto3Storage"
+    STATICFILES_STORAGE = "wedding_res.storage_backends.StaticToS3Storage"
+
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
