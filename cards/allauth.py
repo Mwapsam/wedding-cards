8a22cb4692aa2f700f562a4ff8edef835edd52b2
Mwapsam <mwapsam@gmail.com>
2025-07-24 11:49:09 +0200

    Update

--- cards/allauth.py
+++ cards/allauth.py
@@ -1,18 +1,21 @@
+# cards/allauth.py
 import typing
-
 from django.contrib import messages
-
 from allauth.account.adapter import DefaultAccountAdapter
-
+from django.utils.text import slugify
+import uuid
 from cards.models import User
 
 
 class AccountAdapter(DefaultAccountAdapter):
+    def generate_unique_username(self, txts, regex=None):
+        email = txts[0] if txts else None
+        phone = txts[1] if len(txts) > 1 else None
 
-    def set_phone(self, user, phone: str, verified: bool):
+    def set_phone(self, user, phone: str, verified: bool = True):
         user.phone = phone
         user.phone_verified = verified
-        user.save(update_fields=["phone", "phone_verified"])
+        user.save(update_fields=["phone"])
 
     def get_phone(self, user) -> typing.Optional[typing.Tuple[str, bool]]:
         if user.phone:
@@ -45,3 +48,14 @@ class AccountAdapter(DefaultAccountAdapter):
 
     def get_user_by_phone(self, phone):
         return User.objects.filter(phone=phone).order_by("-phone_verified").first()
+
+    def save_user(self, request, user, form, commit=True):
+        user = super().save_user(request, user, form, commit=False)
+        user.email = form.cleaned_data.get("email", "")
+        user.phone = form.cleaned_data.get("phone", "")
+        user.first_name = form.cleaned_data.get("first_name", "")
+        user.last_name = form.cleaned_data.get("last_name", "")
+        user.phone_verified = True
+        if commit:
+            user.save()
+        return user
--- cards/forms.py
+++ cards/forms.py
@@ -70,6 +70,7 @@ class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
         user.email = self.cleaned_data.get("email")
+        user.phone_verified = True
         user.phone = self.cleaned_data.get("phone")
         user.first_name = self.cleaned_data.get("first_name")
         user.last_name = self.cleaned_data.get("last_name")
