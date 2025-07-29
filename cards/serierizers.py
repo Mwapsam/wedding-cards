2b7b4264c2c3fefbcaa4e40f474fa511bb0ac283
Mwapsam <mwapsam@gmail.com>
2025-07-29 15:50:51 +0200

    Update

--- cards/serierizers.py
+++ cards/serierizers.py
@@ -1,22 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+
 class CustomAuthTokenSerializer(serializers.Serializer):
-    email = serializers.CharField(required=False, allow_null=True)
-    phone = serializers.CharField(required=False, allow_null=True)
-    password = serializers.CharField(write_only=True)
+    email = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    phone = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    password = serializers.CharField(write_only=True, required=True)
 
     def validate(self, data):
-        email = data.get("email")
-        phone = data.get("phone")
+        email = data.get("email", "").strip()
+        phone = data.get("phone", "").strip()
         password = data.get("password")
 
-        if not (email or phone):
+        if not email and not phone:
             raise serializers.ValidationError(
-                _("Either email or phone number must be provided.")
+                {
+                    "field_errors": {
+                        "email": [_("Either email or phone number must be provided.")],
+                        "phone": [_("Either email or phone number must be provided.")],
+                    }
+                }
             )
 
         user = None
@@ -27,6 +34,11 @@ class CustomAuthTokenSerializer(serializers.Serializer):
 
         if user and user.check_password(password):
             if not user.is_active:
-                raise serializers.ValidationError(_("User account is disabled."))
-            return {"user": user}
-        raise serializers.ValidationError(_("Invalid credentials."))
+                raise serializers.ValidationError(
+                    {"non_field_errors": [_("User account is disabled.")]}
+                )
+            token, created = Token.objects.get_or_create(user=user)
+            return {"user": user, "token": token.key}
+        raise serializers.ValidationError(
+            {"non_field_errors": [_("Invalid credentials.")]}
+        )
